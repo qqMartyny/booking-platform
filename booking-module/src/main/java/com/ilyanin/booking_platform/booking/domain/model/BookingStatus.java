1a6dd0dcae0ba9c365d651ff41fdad58a4b00986
martyny <qqmartyny@gmail.com>
2026-03-21 10:30:00 +0300

    feat(booking-module): add BookingStatus enum

---
+++ booking-module/src/main/java/com/ilyanin/booking_platform/booking/domain/model/BookingStatus.java
@@ -0,0 +1,10 @@
+package com.ilyanin.booking_platform.booking.domain.model;
+
+public enum BookingStatus {
+    PENDING,
+    APPROVED,
+    REJECTED,
+    CANCELLEDб,
+    CHANGE_REQUESTED,
+    COMPLETED
+}
