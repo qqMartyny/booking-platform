9948a135ec820feffabdc3b788312243af3cb47a
martyny <qqmartyny@gmail.com>
2026-03-22 20:46:04 +0300

    feat(booking): add fields and private constructor for Booking class

---
+++ booking-module/src/main/java/com/ilyanin/booking_platform/booking/domain/model/Booking.java
@@ -0,0 +1,50 @@
+package com.ilyanin.booking_platform.booking.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import com.ilyanin.booking_platform.booking.domain.event.DomainEvent;
+import com.ilyanin.booking_platform.shared.DateRange;
+import com.ilyanin.booking_platform.shared.Money;
+
+public class Booking {
+
+    private final UUID id;
+    private final UUID guestId;
+    private final UUID roomId;
+    private final DateRange dateRange;
+    private final Money totalPrice;
+    private final LocalDateTime createdAt;
+    private BookingStatus status;
+    private final List<DomainEvent> domainEvents = new ArrayList<>();
+
+    private Booking(
+        UUID id,
+        UUID guestId,
+        UUID roomId,
+        DateRange dateRange,
+        BookingStatus status,
+        Money totalPrice,
+        LocalDateTime createdAt
+        ) {
+        this.id = id;
+        this.guestId = guestId;
+        this.roomId = roomId;
+        this.dateRange = dateRange;
+        this.status = status;
+        this.totalPrice = totalPrice;
+        this.createdAt = createdAt;
+    }
+
+    public static Booking create(
+        UUID guestId, 
+        UUID roomId, 
+        DateRange dateRange, 
+        Money totalPrice
+    ) {
+
+        return new Booking(roomId, guestId, roomId, dateRange, null, totalPrice, null);
+    }
+}
