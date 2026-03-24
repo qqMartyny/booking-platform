b73b3f89b508943cb965cd066f47caca452ddc3a
martyny <qqmartyny@gmail.com>
2026-03-24 17:03:06 +0300

    refactor(shared-kernel): refactor DomainEvent from interface to abstract class

--- shared-kernel/src/main/java/com/ilyanin/booking_platform/shared/event/DomainEvent.java
+++ shared-kernel/src/main/java/com/ilyanin/booking_platform/shared/event/DomainEvent.java
@@ -3,9 +3,19 @@ package com.ilyanin.booking_platform.shared.event;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public interface DomainEvent {
-    
-    LocalDateTime occurredAt();
-    UUID aggregateId();
-    UUID eventId();
+public abstract class DomainEvent {
+
+    private final UUID eventId;
+    private final UUID agregateId;
+    private final LocalDateTime occuredAt;
+
+    protected DomainEvent(
+        UUID eventId, 
+        UUID agregateId, 
+        LocalDateTime occuredAt
+    ) {
+        this.eventId = eventId;
+        this.agregateId = agregateId;
+        this.occuredAt = occuredAt;
+    }
 }
