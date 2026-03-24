d44dee077a31d16880c2c561641c7aaa2cf0e3ac
martyny <qqmartyny@gmail.com>
2026-03-25 01:04:52 +0300

    feat(booking): create BookingJpaRepository

---
+++ booking-module/src/main/java/com/ilyanin/booking_platform/booking/adapter/out/persistence/BookingJpaRepository.java
@@ -0,0 +1,31 @@
+package com.ilyanin.booking_platform.booking.adapter.out.persistence;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.ilyanin.booking_platform.booking.domain.model.BookingStatus;
+
+public interface BookingJpaRepository extends 
+    JpaRepository<BookingJpaEntity, UUID>,
+    JpaSpecificationExecutor<BookingJpaEntity> {
+    
+    @Query("""        
+        SELECT b FROM BookingJpaEntity b
+        WHERE b.roomId = :roomId
+        AND :startDate < b.endDate
+        AND b.startDate < :endDate
+        AND :status = b.status
+    """)
+    List<BookingJpaEntity> findConflicting(
+        @Param("roomId") UUID roomId,
+        @Param("startDate") LocalDate startDate,
+        @Param("endDate") LocalDate endDate,
+        @Param("status") BookingStatus status
+    );
+}
