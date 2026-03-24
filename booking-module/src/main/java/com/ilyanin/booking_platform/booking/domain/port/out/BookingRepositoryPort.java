c606b6a2e1fb1baf741a7e1d157c0a92e258f5a4
martyny <qqmartyny@gmail.com>
2026-03-24 18:53:23 +0300

    feat(booking): add status parameter for findConflicting method in BookingRepositoryPort

--- booking-module/src/main/java/com/ilyanin/booking_platform/booking/domain/port/out/BookingRepositoryPort.java
+++ booking-module/src/main/java/com/ilyanin/booking_platform/booking/domain/port/out/BookingRepositoryPort.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import com.ilyanin.booking_platform.booking.domain.model.Booking;
+import com.ilyanin.booking_platform.booking.domain.model.BookingStatus;
 import com.ilyanin.booking_platform.booking.domain.port.in.BookingSearchFilter;
 import com.ilyanin.booking_platform.shared.DateRange;
 import com.ilyanin.booking_platform.shared.PageResult;
@@ -12,6 +13,10 @@ import com.ilyanin.booking_platform.shared.PageResult;
 public interface BookingRepositoryPort {
     Booking save(Booking booking);
     Optional<Booking> findById(UUID id);
-    List<Booking> findConflicting(UUID roomId, DateRange dateRange);
+    List<Booking> findConflicting(
+        UUID roomId, 
+        DateRange dateRange, 
+        BookingStatus status
+    );
     PageResult<Booking> findAll(BookingSearchFilter filter);
 }
