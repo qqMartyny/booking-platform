8146e7fcf42805733761d2ee53ebab368e9e2103
martyny <qqmartyny@gmail.com>
2026-03-25 18:19:36 +0300

    feat(booking/adapter): create CreateBookingRequest DTO

---
+++ booking-module/src/main/java/com/ilyanin/booking_platform/booking/adapter/in/rest/dto/request_dto/CreateBookingRequest.java
@@ -0,0 +1,21 @@
+package com.ilyanin.booking_platform.booking.adapter.in.rest.dto.request_dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Currency;
+import java.util.UUID;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record CreateBookingRequest(
+    @NotNull UUID guestId,
+    @NotNull UUID roomId,
+    @NotNull @FutureOrPresent LocalDate startDate,
+    @NotNull @FutureOrPresent LocalDate endDate,
+    @NotNull @Positive BigDecimal amount,
+    @NotNull Currency currency
+) {
+
+}
