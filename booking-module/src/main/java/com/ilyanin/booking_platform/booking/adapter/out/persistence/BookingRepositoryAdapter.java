81b4667eab49504fb299fcd675669dd2b0495af5
martyny <qqmartyny@gmail.com>
2026-03-25 17:50:03 +0300

    feat(booking/adapter): add findAll method to adapter

--- booking-module/src/main/java/com/ilyanin/booking_platform/booking/adapter/out/persistence/BookingRepositoryAdapter.java
+++ booking-module/src/main/java/com/ilyanin/booking_platform/booking/adapter/out/persistence/BookingRepositoryAdapter.java
@@ -4,6 +4,12 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+
 import com.ilyanin.booking_platform.booking.domain.model.Booking;
 import com.ilyanin.booking_platform.booking.domain.model.BookingStatus;
 import com.ilyanin.booking_platform.booking.domain.port.in.BookingSearchFilter;
@@ -11,6 +17,7 @@ import com.ilyanin.booking_platform.booking.domain.port.out.BookingRepositoryPor
 import com.ilyanin.booking_platform.shared.DateRange;
 import com.ilyanin.booking_platform.shared.PageResult;
 
+@Component
 public class BookingRepositoryAdapter implements BookingRepositoryPort{
 
     private final BookingMapper mapper;
@@ -48,8 +55,40 @@ public class BookingRepositoryAdapter implements BookingRepositoryPort{
 
     @Override
     public PageResult<Booking> findAll(BookingSearchFilter filter) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+
+        Specification<BookingJpaEntity> spec = Specification.unrestricted();
+
+        if (filter.guestId() != null) {
+            spec = spec.and((root, query, cb) ->
+                cb.equal(root.get("guestId"), filter.guestId())
+            );
+        }
+
+        if (filter.roomId() != null) {
+            spec = spec.and((root, query, cb) ->
+                cb.equal(root.get("roomId"), filter.roomId())
+            );
+        }
+        
+        if (filter.status() != null) {
+            spec = spec.and((root, query, cb) ->
+                cb.equal(root.get("status"), filter.status())
+            );
+        }
+
+        Pageable pageable = PageRequest.of(filter.page(), filter.pageSize());
+        Page<BookingJpaEntity> page = repository.findAll(spec, pageable);
+
+        List<Booking> content = page.getContent()
+            .stream()
+            .map(mapper::toDomain)
+            .toList();
+
+        return new PageResult<Booking>(
+            content, 
+            filter.page(), 
+            filter.pageSize(), 
+            page.getTotalElements());
     }
     
 }
