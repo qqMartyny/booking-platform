b6c8d0bf83bdc5193035bb2928842502515190bc
martyny <qqmartyny@gmail.com>
2026-03-28 19:33:32 +0300

    feat(property): create main events

---
+++ property-module/src/main/java/com/ilyanin/booking_platform/property/domain/event/PropertyAvailabilityBolockedEvent.java
@@ -0,0 +1,5 @@
+package com.ilyanin.booking_platform.property.domain.event;
+
+public class PropertyAvailabilityBolockedEvent {
+
+}
---
+++ property-module/src/main/java/com/ilyanin/booking_platform/property/domain/event/PropertyCreatedEvent.java
@@ -0,0 +1,26 @@
+package com.ilyanin.booking_platform.property.domain.event;
+
+import java.util.UUID;
+
+import com.ilyanin.booking_platform.property.domain.model.Address;
+import com.ilyanin.booking_platform.shared.event.DomainEvent;
+
+public class PropertyCreatedEvent extends DomainEvent {
+
+    
+    private final UUID hostId;
+    private final String name;
+    private final Address address;
+
+    public PropertyCreatedEvent(
+        UUID aggregateId,
+        UUID hostId,
+        String name,
+        Address address
+    ) {
+        super(aggregateId);
+        this.hostId = hostId;
+        this.name = name;
+        this.address = address;
+    }
+}
